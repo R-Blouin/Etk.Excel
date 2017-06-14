35a264fbcc6b5ba111af32540712bebf9721a004
rblouin051512 <richard.blouin-ext@lyxor.com>
2017-06-14 19:01:09 +0200

    ...

--- Demos/Etk.Demo.ExcelDna1/AddinManager.cs
+++ Demos/Etk.Demo.ExcelDna1/AddinManager.cs
@@ -1,6 +1,9 @@
-﻿using Etk.Excel;
+﻿using System.IO;
+using Etk.Demos.Data.Shops;
+using Etk.Excel;
 using ExcelDna.Integration;
 using ExcelInterop = Microsoft.Office.Interop.Excel;
+using Etk.Excel.BindingTemplates.Views;
 
 namespace Etk.Demo.ExcelDna1
 {
@@ -19,6 +22,19 @@ namespace Etk.Demo.ExcelDna1
 
             // Init the ETK Framework : mandatory before any uses of the framework
             ETKExcel.Init(ExcelApplication);
+
+            ExcelInterop.Worksheet worksheetDestination = ETKExcel.ExcelApplication.GetWorkSheetFromName(currentWorkbook, "Results");
+
+            string addinPath = (string)XlCall.Excel(XlCall.xlGetName);
+            string rootPath = Path.GetDirectoryName(addinPath);
+            ExcelInterop.Workbooks workbooks = ETKExcel.ExcelApplication.Application.Workbooks;
+            ExcelInterop.Workbook workbookTemplateContainer = workbooks.Open(Path.Combine(rootPath, "Etk.Demo.Templates.xlsx"), true, true);
+
+            ExcelInterop.Worksheet worksheetTemplateSource = ETKExcel.ExcelApplication.GetWorkSheetFromName(workbookTemplateContainer, "Templates");
+            ExcelInterop.Range viewFirstOutputRange = worksheetDestination.Range["B2"];
+            IExcelTemplateView mainview  = ETKExcel.TemplateManager.AddView(worksheetTemplateSource, "Main", worksheetDestination, viewFirstOutputRange);
+            mainview.SetDataSource(ShopManager.Shops);
+            mainview.Render();
         }
 
         public void AutoClose()
--- Demos/Etk.Demo.ExcelDna1/Properties/AssemblyInfo.cs
+++ Demos/Etk.Demo.ExcelDna1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Etk.Tests.Templates.ExcelDna1")]
+[assembly: AssemblyTitle("Etk.Demo.ExcelDna1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Etk.Tests.Templates.ExcelDna1")]
+[assembly: AssemblyProduct("Etk.Demo.ExcelDna1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
