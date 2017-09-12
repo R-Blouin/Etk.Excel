dc76697b5cbaeefc7082670c08217ba5f15916c4
Rbl <blouinrichard@free.fr>
2017-09-13 01:01:15 +0200

    Forgot 2 files

---
+++ Etk.Excel/BindingTemplates/Definitions/EventExcelCallbacksManager.cs
@@ -0,0 +1,27 @@
+﻿using Etk.BindingTemplates.Context;
+using Etk.BindingTemplates.Definitions;
+using Etk.BindingTemplates.Definitions.EventCallBacks;
+using System.ComponentModel.Composition;
+using System.Runtime.InteropServices;
+
+namespace Etk.Excel.BindingTemplates.Definitions
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class EventExcelCallbacksManager : EventCallbacksManager
+    {
+        public static void ComInvoke(IBindingContextElement catchingContextElement, object catchingDataSource, IBindingContextItem currentContextItem, object currentContextItemDataSource)
+        {
+            ExcelTemplateDefinition templateDefinition = catchingContextElement.ParentPart.ParentContext.TemplateDefinition as ExcelTemplateDefinition;
+            try
+            {
+                ETKExcel.ExcelApplication.ExecuteVbaMAcro(templateDefinition.SelectionChangedComFonctionName, new[] { catchingDataSource, currentContextItem.BindingDefinition.Name });
+            }
+            catch (COMException ex)
+            {
+                if (ex.ErrorCode != (int) SpecificException.DISP_E_UNKNOWNNAME)
+                    throw;
+            }
+        }
+    }
+}
---
+++ Etk/BindingTemplates/Definitions/SpecificException.cs
@@ -0,0 +1,7 @@
+﻿namespace Etk.BindingTemplates.Definitions
+{
+    public enum SpecificException
+    {
+        DISP_E_UNKNOWNNAME = -2147352570
+    }
+}
